b64add17218e681382530efa187767860052fdea
Laf <f.lardieri@honegger.ch>
2021-02-10 21:04:47 +0100

    Added dummy test for now

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,6 @@
 // Import without specifying the extension. `./Lib.js` would look odd in a .ts file
-import { Lib } from './Lib';
 
 test('constructor', () => {
-  const instance = new Lib();
-  expect(instance instanceof Lib).toBeTruthy();
-});
-
-try {
-  throw new Error('testing source mapping in Jest');
-} catch (e) {
-  if (e.stack.includes('.ts:')) {
-    console.info('Stack trace mapped back to TypeScript should indicate correct error line number');
-    console.info(e);
-  }
-}
+  const emptyString = ''
+  expect(emptyString === '').toBeTruthy()
+})
